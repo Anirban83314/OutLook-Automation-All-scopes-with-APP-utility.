e4ab79c5908a52d365bd30bb5db14c54b1fc5cef
Ozzius <deb991@users.noreply.github.com>
2017-12-28 19:31:44 +0530

    Data evaluated as per requirement.
    Data evaluated as per requirement.

--- ack1_FPS_Inbox_Items.py
+++ ack1_FPS_Inbox_Items.py
@@ -1,23 +1,23 @@
 import win32com.client as win32
 
-from FPSAlertMonitor import FPS_Mail_subject , FPS_Mail_body
+from MasterAlertMonitor import _Mail_subject , _Mail_body
 
 
 def AckMailSend():
     outlook = win32.Dispatch ( 'outlook.application' )
     mail = outlook.CreateItem ( 0 )
-    # mail.To = ''
-    # mail.Cc = 'GXSOMWIPRODSMonitoringservice@shell.com; anirban.a.ghosh@shell.com'
-    mail.Bcc = 'debashis.biswas@shell.com'
-    mail.Subject = 'Acknowledgement || Re: ' + FPS_Mail_subject
+    # mail.To = 'recepeints'
+    # mail.Cc = 'recepeints; recepeints'
+    mail.Bcc = 'recepeints'
+    mail.Subject = 'Acknowledgement || Re: ' + _Mail_subject
 
-    # attachment = (os.path.expanduser(os.getenv('USERPROFILE')) + '\\Desktop\\AUTOMATION\\Logs\\loadddf.txt')
+    # attachment = (os.path.expanduser(os.getenv('USERPROFILE')) + '\\path\\to\\the\\attachment\\')
     # mail.Attachments.Add(attachment)
 
-    # F = open(os.path.expanduser(os.getenv('USERPROFILE')) + '\\Desktop\\AUTOMATION\\Logs\\loadddf.txt')
+    # F = open(os.path.expanduser(os.getenv('USERPROFILE')) + '\\path\\to\\the\\attachment\\')
     # line = F.read()
 
-    (mail.body) = 'Due to cyclic Job, it will be executed successfully in the next Execution Cycle.' + (FPS_Mail_body)
+    (mail.body) = 'Due to cyclic Job, it will be executed successfully in the next Execution Cycle.' + (_Mail_body)
 
     mail.send
 
