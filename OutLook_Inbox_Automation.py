bd1366d68adbe02db219bc4a12dbc97a7036ae86
Ozzius <deb991@users.noreply.github.com>
2018-07-21 04:34:35 +0530

    Program is ready & launched.
    Program is ready & launched.

--- OutLook_Inbox_Automation.py
+++ OutLook_Inbox_Automation.py
@@ -1,16 +1,73 @@
-#!/usr/bin/env python
+__project__ = 'OSADOL.'
+__author__ = 'Jeys_Ozzius.'
+__descript__ = "Copyright (c) 2017 Anirban Ghosh(Anirban83314) & Debashis Biswas(deb991)."
+__URL__ = 'https://github.com/Anirban83314/OutLook-Automation-All-scopes-with-APP-utility.'
+__NB__ = 'For more information, please see github page & all commit details.'
+__CipherSig__ = 'This project & all associate files are encrypted under PBEncryption cryptography. Another details will be available at the end of this pgogram.'
+
+import os
+import sys
 import win32com.client
+import datetime as dt
 
-outlook = win32com.client.Dispatch("Outlook.application").GetNamespace("MAPI")
+outlook = win32com.client.Dispatch("Outlook.application")
+mapi = outlook.GetNamespace("MAPI")
 
-###"6" refers to the index of a folder - in this case, the inbox. You can change that number to reference any other folder##
-inbox = outlook.GetDefaultFolder(6)
+date_time = dt.datetime.now()
+lastQrtDateTime = dt.datetime.now() - dt.timedelta(minutes = 30)
 
-messages = inbox.Items
-message = messages.GetLast()
-Message_subject = message.subject
-Message_body = message.body
+Monitoring_Mails = mapi.Folders['Debashis.Biswas@xyz.com'].Folders['Inbox'].Folders['xyz Monitoring service']
+File_Mover_Alerts = mapi.Folders['Debashis.Biswas@xyz.com'].Folders['Inbox'].Folders['File_Mover_Alert_Mail_Folder']
 
-print(message)
-print(Message_subject)
-print(Message_body)
+#-----------------Monitoring Items--------------#
+MonitoringMails = Monitoring_Mails.Items
+MonitoringMails.Sort("[ReceivedTime]", True)
+MonitoringMail = MonitoringMails.GetFirst()
+MonitoringMail_Count = MonitoringMails.count
+MonitoringMail_subject = MonitoringMail.Subject
+MonitoringMail_body = MonitoringMail.body
+lastQtrMessages = MonitoringMails.Restrict("[ReceivedTime] >= '" +lastQrtDateTime.strftime('%m/%d/%Y %H:%M %p')+"'")
+
+print('/n')
+print(MonitoringMail)
+print(MonitoringMail_subject)
+print(MonitoringMail_body)
+
+print('>>>>>>>>>Monitoring Mails Alert<<<<<<<<')
+print('/n')
+
+
+for MonitoringMail in lastQtrMessages:
+    print(MonitoringMail_subject)
+    print(MonitoringMail.ReceivedTime)
+    MonitoringMail = lastQtrMessages.GetFirst()
+    MonitoringMail_iter = lastQtrMessages.GetNext()
+    print('/n')
+
+while MonitoringMail:
+
+    for MonitoringMail in lastQtrMessages:
+        if MonitoringMail.Unread == True:
+
+            print('Initiating Main process -- flag 1.1')
+            if MonitoringMail_subject == 'UnTrusted Message :XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX':
+                print('Initiaitng Shell drive partner auto-acknowledgement mail. ')
+                exec(open("D:\\OutLook Mail Automation\\xxxxxxxxxxxxxxMailACK.py").read())
+                MonitoringMail.Unread == False
+                pass
+
+
+            elif MonitoringMail_subject == 'Something else will be there !!!!':
+                print('Bang on again!!!')
+                pass
+
+            else:
+                print('Nothing important in out Monitoring DL ~!!!!!!!!~~~')
+
+
+
+            break
+        break
+    break
+
+print('EOF')
